a5c4a1139c4a2719075c3a4ec80eff8b7c4f3682
Drako <dragon12xw@gmail.com>
2021-04-11 03:26:52 -0500

    responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MenuHComponent } from './menu-h/menu-h.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    MenuHComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
