c1c1485a1ac84866c824a731216990d46e4cf73e
Drako <dragon12xw@gmail.com>
2021-04-16 03:10:56 -0500

    menu h ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MenuHComponent } from './menu-h/menu-h.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu-h/menu-h.component.ts
+++ src/app/menu-h/menu-h.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {  Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-h',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-h.component.css']
 })
 export class MenuHComponent implements OnInit {
+  toogle2: string = 'close';
+  toogle1: string = 'open';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
+
+  isCh(){
+    if (this.toogle1 === 'open') {
+      this.toogle1 = 'close'
+      this.toogle2 = 'open'
+    } else if (this.toogle1 === 'close')
+    {
+      this.toogle1 = 'open'
+      this.toogle2 = 'close'
+    }
+  }
+
 
 }
